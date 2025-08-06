1af2e2994ce52ba440adf7ca1e2b1850fe2f3c49
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-08-06 14:46:03 -0300

    adicionar exercicio ContadorRunnable

--- src/main/java/org/example/topic1_fundamentals/ContadorRunnable.java
+++ src/main/java/org/example/topic1_fundamentals/ContadorRunnable.java
@@ -2,11 +2,11 @@ package org.example.topic1_fundamentals;
 
 /**
  * Exercício 1.1: Contador Básico com Runnable
- *
+ * <p>
  * Objetivo: Criar uma classe que implementa Runnable e que, ao ser executada,
  * imprime os números de 1 a 10, fazendo uma pausa de 100 milissegundos
  * entre cada número.
- *
+ * <p>
  * Passos:
  * 1. Crie uma classe chamada `ContadorRunnable`.
  * 2. Implemente a interface `Runnable`.
@@ -17,4 +17,34 @@ package org.example.topic1_fundamentals;
  */
 
 public class ContadorRunnable {
+
+    public static void main(String[] args) {
+
+        MeuRunabble meuRunabble = new MeuRunabble();
+
+        Thread thread = new Thread(meuRunabble);
+
+        thread.start();
+    }
+
+    public static class MeuRunabble implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++){
+                String nomeThread = Thread.currentThread().getName();
+                System.out.println("Número: " + i + " - Thread: " + nomeThread);
+                sleep();
+            }
+
+        }
+    }
+
+    public static void sleep() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
 }
