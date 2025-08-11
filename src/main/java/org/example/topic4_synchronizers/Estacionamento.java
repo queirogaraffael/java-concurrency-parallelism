0bf0160dd6c23bd918aa37fc6fc9c101ad5ee5e9
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-08-11 10:37:24 -0300

    adicionar exercicio Estacionamento

--- src/main/java/org/example/topic4_synchronizers/Estacionamento.java
+++ src/main/java/org/example/topic4_synchronizers/Estacionamento.java
@@ -1,10 +1,14 @@
 package org.example.topic4_synchronizers;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 /**
  * Exercício 4.1: Semaphore
- *
+ * <p>
  * Objetivo: Simular um recurso com capacidade limitada (um estacionamento).
- *
+ * <p>
  * Passos:
  * 1. Crie uma classe `Estacionamento` com um `Semaphore` inicializado com 3 permissões (vagas).
  * 2. Crie uma `Runnable` que representa um carro. No método `run()`:
@@ -17,4 +21,59 @@ package org.example.topic4_synchronizers;
  * 4. Observe como apenas 3 carros conseguem entrar por vez.
  */
 public class Estacionamento {
+
+    public static final Semaphore SEMAFORO = new Semaphore(3);
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = null;
+
+        Runnable carro = () -> {
+            acquire();
+
+            System.out.println(Thread.currentThread().getName() + " entrou no estacionamento");
+
+            sleep(3);
+
+            System.out.println(Thread.currentThread().getName() + " saiu do estacionamento");
+
+            SEMAFORO.release();
+        };
+
+
+        try {
+            executor = Executors.newCachedThreadPool();
+
+            for (int i = 0; i < 10; i++) {
+                executor.submit(carro);
+            }
+
+        } finally {
+            if (executor != null) {
+                executor.shutdown();
+            }
+        }
+
+    }
+
+    public static void acquire() {
+        try {
+            SEMAFORO.acquire();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Thread interrompida durante acquire: " + e.getMessage());
+        }
+    }
+
+    public static void sleep(int segundos) {
+
+        try {
+            Thread.sleep(segundos * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println(e.getMessage());
+        }
+    }
+
+
 }
