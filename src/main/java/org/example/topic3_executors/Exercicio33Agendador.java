062dfbfacae0e4eb982ef826a66ca344d4fa26fb
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-08-11 09:56:05 -0300

    adicionar exercicio Exercicio33Agendador

--- src/main/java/org/example/topic3_executors/Exercicio33Agendador.java
+++ src/main/java/org/example/topic3_executors/Exercicio33Agendador.java
@@ -1,6 +1,11 @@
 package org.example.topic3_executors;
 
 
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Exercício 3.3 (Agendamento):
  * Objetivo: Agendar uma tarefa para execução periódica.
@@ -9,8 +14,42 @@ package org.example.topic3_executors;
  * 2. Crie uma `Runnable` que imprime a hora atual (`LocalTime.now()`).
  * 3. Agende a tarefa para ser executada a cada 5 segundos, com um atraso inicial de 2 segundos,
  * usando `scheduleAtFixedRate()`.
+ *
  * 4. Deixe a thread `main` dormir por 20 segundos para ver a tarefa rodando, e depois
  * desligue o executor.
  */
 public class Exercicio33Agendador {
+
+
+    public static void main(String[] args) {
+
+        ScheduledExecutorService executor = null;
+
+        Runnable tarefa = () -> {
+            System.out.println(LocalTime.now());
+        };
+
+        try{
+            executor = Executors.newSingleThreadScheduledExecutor();
+
+            executor.scheduleAtFixedRate(tarefa, 2, 5, TimeUnit.SECONDS);
+
+            sleep(20);
+
+        }finally {
+            if(executor!= null){
+                executor.shutdown();
+            }
+        }
+
+    }
+
+    public static void sleep(int seconds){
+        try{
+            Thread.sleep(seconds * 1000L);
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            System.err.println(e.getMessage());
+        }
+    }
 }
